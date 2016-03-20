b33a736da27bf05f59247e8c28d4c1ad133e533c
Carlos A. Bonilla <cbonilla@gen9bio.com>
2016-03-20 15:52:29 -0400

    switch to a more defined data structure

--- scan_media.py
+++ scan_media.py
@@ -21,59 +21,80 @@ def hash_file(path):
         md5 = hashlib.md5(ss).hexdigest()
     return sha1 + md5
 
-def add_media(mem, fullpath):
-    mem['counter'] += 1
-    if mem['counter'] % 1000 == 0:
-        print "{0} files scanned".format(mem['counter'])
-    hash = hash_file(fullpath)
-    mem['media'].setdefault(hash, []).append(fullpath)
-    return mem
+class mediaStruct(object):
+    """the data structure for the media being operated on"""
+    def __init__(self, media={}, counter=None, source=''):
+        if counter:
+            self.counter = counter
+        else:
+            self.counter = sum(map(len, media.values()))
+        self.media = media
+        self.source = source
 
-def load(input_file):
-    with open(input_file) as f:
-        data = json.loads(f.read())
-    return data
+    def add_media(fullpath):
+        self.counter += 1
+        if self.counter % 1000 == 0:
+            print "{0} files scanned".format(self.counter)
+        hash = hash_file(fullpath)
+        self.media.setdefault(hash, []).append(fullpath)
 
-def save(data, output_file):
-    with open(output_file, 'w') as f:
-        f.write(json.dumps(data))
+    @classmethod
+    def load(cls, input_file):
+        with open(input_file) as f:
+            data = json.loads(f.read())
+        return cls(data['media'], data['counter'], source=input_file)
 
-def merge(a, b):
-    c = {'counter': a['counter'] + b['counter']}
-    photos = filter(lambda (key, val): key != counter, a.items() + b.items())
-    combine = lambda (key, val): c.setdefault(key, []).append(val)
-    map(combine, photos)
-    return c
+    def save(self, output_file):
+        if output_file[-5:].lower() != '.json':
+            output_file += '.json'
+        with open(output_file, 'w') as f:
+            f.write(json.dumps({
+                'counter': self.counter,
+                'media': self.media
+                }))
+        self.source = output_file
 
-def duplicates(data):
-    hasDup = lambda val: len(val) > 1
-    dups = toolz.valfilter(hasDup, data['media'])
-    return {'counter': sum(map(len, dups.values())), 'media': dups}
+    def merge(self, b):
+        counter = self.counter + b.counter
+        media = {}
+        photos = self.media.items() + b.media.items()
+        combine = lambda (key, val): media.setdefault(key, []).append(val)
+        map(combine, photos)
+        return mediaStruct(media, counter)
 
-def delete_all(data):
-    files = toolz.concatv(data['media'].values())
-    map(os.remove, files)
+    def duplicates(self):
+        hasDup = lambda val: len(val) > 1
+        dups = toolz.valfilter(hasDup, self.media)
+        return mediaStruct(dups)
 
-def subtract(A, B):
-    a = set(A['media'].keys())
-    b = set(B['media'].keys())
-    keys = a - b
-    media = toolz.keyfilter(lambda key: key in keys, A['media'])
-    counter = sum(map(len, media.values()))
-    return {'counter': counter, 'media': media}
+    def delete_all(self):
+        if raw_input('are you sure? (y/n) ') == 'y':
+            files = toolz.concatv(self.media.values())
+            for i in files:
+                os.remove(i)
+                print 'deleted:', i
 
-def make_links(dest_path, i, paths):
-    'create hard links in dest_path with all the contents, for easy comparison'
-    dest = '{0}/{1}'.format(dest_path, i)
-    map(lambda (j, path): os.link(path, '{0}_img_{1}.{2}'.format(dest, j, path[-3:])), enumerate(paths))
-    with open('{0}/files.txt'.format(dest_path), 'a') as f:
-        f.write('\n'.join(paths) + '\n')
+    def __sub__(self, B):
+        a = set(self.media.keys())
+        b = set(B.media.keys())
+        keys = a - b
+        media = toolz.keyfilter(lambda key: key in keys, self.media)
+        return mediaStruct(media)
 
-def explore_media(data, dest):
-    os.mkdir(dest)
-    for i, paths in enumerate(data['media'].values()):
-        make_links(dest, i, paths)
+    def _make_links(self, dest_path, i, paths):
+        'create hard links in dest_path with all the contents, for easy comparison'
+        dest = '{0}/{1}'.format(dest_path, i)
+        for j, path in enumerate(paths):
+            os.link(path, '{0}_img_{1}.{2}'.format(dest, j, path[-3:]))
 
+    def explore_media(self):
+        if self.source:
+            dest = self.source[:-4] + 'files'
+            os.mkdir(dest)
+            for i, paths in enumerate(data.media.values()):
+                make_links(dest, i, paths)
+        else:
+            print 'you must save before exploring this media\n'
 
 def parse_cli():
     parser = argparse.ArgumentParser(usage="usage: %(prog)s")
@@ -91,8 +112,10 @@ def main():
     file_list = filter(None, toolz.mapcat(iterFiles, os.walk(args.base_path)))
     print '{0} total files'.format(len(file_list))
 
-    image_scan = reduce(add_media, file_list, {'counter': 0, 'media': {}})
-    save(image_scan, args.output_file)
+    image_scan = mediaStruct()
+    for media in file_list:
+        image_scan.add_media(media)
+    image_scan.save(args.output_file)
 
 if __name__ == '__main__':
     main()
